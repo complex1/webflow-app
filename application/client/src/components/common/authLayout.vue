9fe8c76ef8242255463b4f26afa9ab01f40453ac
shubham <shubham.m@flexehr.com>
2025-05-28 20:52:24 +0530

    Add SVG assets for background pattern and logo
    - Created a new SVG file for a background pattern with a simple rectangle design.
    - Added a detailed SVG logo with multiple paths and gradients, including animation for the stroke.

--- application/client/src/components/common/authLayout.vue
+++ application/client/src/components/common/authLayout.vue
@@ -1,15 +1,18 @@
 <template>
   <div class="flex-center fh fw bg-light register-page">
-    <div class="p-l flex-center">
+   <div class="background">
+     <div v-for="i in count" :key="i" class="box"></div>
+   </div>
+    <div class="p-l flex-center z-1">
       <img width="100%" class="round-3" src="../../assets/logo.svg" alt="" />
     </div>
-    <div class="flex-center">
+    <div class="flex-center z-1">
       <slot></slot>
     </div>
   </div>
 </template>
 <script lang="ts">
-import { defineComponent } from 'vue';
+import { defineComponent, ref } from 'vue';
 
 export default defineComponent({
   name: 'AuthLayout',
@@ -18,6 +21,12 @@ export default defineComponent({
       type: String,
       default: 'Authentication'
     }
+  },
+  setup() {
+    const count = ref(0);
+
+    count.value = Math.floor(((window.innerWidth / 50) * (window.innerHeight / 50)) * 1.5);
+    return { count };
   }
 });
 </script>
@@ -26,10 +35,42 @@ export default defineComponent({
   display: grid;
   grid-template-columns: 1fr 1fr;
   height: 100vh;
-  // background: linear-gradient(to right, #ffcece, #e0e7ff);
-
-  // & img {
-  //   box-shadow: 0px 0px 12px #e7e7e7;
-  // }
+  width: 100vw;
+  .background {
+    position: fixed;
+    width: 100vw;
+    height: 100vh;
+    overflow: hidden;
+    top: 0;
+    left: 0;
+    z-index: 0;
+    display: flex;
+    flex-wrap: wrap;
+  }
+  .box {
+    height: 50px;
+    width: 50px;
+    position: relative;
+    &::before {
+      content: '';
+      position: absolute;
+      top: 50%;
+      left: 0;
+      right: 0;
+      border: 1px solid var(--color-primary);
+      transform: translateY(-50%);
+      opacity: 0.1;
+    }
+    &::after {
+      content: '';
+      position: absolute;
+      top: 0;
+      left: 50%;
+      bottom: 0;
+      border: 1px solid var(--color-primary);
+      transform: translateX(-50%);
+      opacity: 0.1;
+    }
+  }
 }
 </style>
