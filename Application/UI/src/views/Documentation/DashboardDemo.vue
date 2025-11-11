54c216e164d5b900a890779a865a8f068ec07fcd
shubham <shubham.m@flexehr.com>
2025-11-11 07:12:36 +0530

    refactor: remove documentation styles and images
    - Deleted documentation CSS file to streamline styles.
    - Removed README.md for documentation images and all associated image files.
    - Cleaned up documentation component exports in index.ts.
    - Updated DashboardDemo.vue to remove unused components.

--- Application/UI/src/views/Documentation/DashboardDemo.vue
+++ Application/UI/src/views/Documentation/DashboardDemo.vue
@@ -1,14 +1,9 @@
 <template>
   <div>
-    <!-- Simple demo page showing how to use dashboard documentation -->
-    <DashboardDocs />
-    <env-file-docs />
   </div>
 </template>
 
 <script setup lang="ts">
-import DashboardDocs from '@/components/documentation/DashboardDocs.vue'
-import EnvFileDocs from '@/components/documentation/EnvFileDocs.vue'
 </script>
 
 <style scoped>
